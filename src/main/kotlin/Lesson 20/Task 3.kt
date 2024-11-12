4d813dcb2e715674e99898e1c1f8fb5bacb54022
ВЧ <kirillkorolev2002@mail.ru>
2024-11-12 17:33:26 +0300

    KS-20-3

---
+++ src/main/kotlin/Lesson 20/Task 3.kt
@@ -0,0 +1,19 @@
+package `Lesson 20`
+
+import kotlin.random.Random
+
+fun main() {
+    val playerKeys = Player(name = "Герой", hasKey = Random.nextBoolean())
+    val checkDoor: (Player) -> String = { player ->
+        if (player.hasKey) {
+            "Игрок открыл дверь"
+        } else {
+            "Дверь заперта"
+        }
+    }
+    println(checkDoor(playerKeys))
+}
+
+class Player(val name: String, var hasKey: Boolean)
+
+
